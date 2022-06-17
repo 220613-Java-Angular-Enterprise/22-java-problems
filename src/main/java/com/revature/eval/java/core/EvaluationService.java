d69a39764a969c031c475f38daa6e35f33c05365
saml5 <samlerch5@gmail.com>
2022-06-17 14:13:10 -0400

    Q1 and Q2 Solution pushed during lecture (Im paying attention)

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -22,7 +22,9 @@ public class EvaluationService {
 
 		public static long toMilesPerHour(double kilometersPerHour) {
 			// TODO Write an implementation for this method declaration
-			return 0;
+			if(kilometersPerHour < 0)
+				return -1;
+			return (long) Math.round((kilometersPerHour * 0.621371));
 		}
 
 		/**
@@ -42,7 +44,10 @@ public class EvaluationService {
 		 */
 		public static String printConversion(double kilometersPerHour) {
 			// TODO Write an implementation for this method declaration
-			return null;
+			if(kilometersPerHour < 0)
+				return "Invalid Value";
+			return kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h";
+			
 		}
 	}
 
