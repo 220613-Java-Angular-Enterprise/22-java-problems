64b361bf94b24f5865ae2ff1d057a0c7b6d3811d
saml5 <samlerch5@gmail.com>
2022-06-20 16:17:46 -0400

    Test Commit

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -391,7 +391,7 @@ public class EvaluationService {
 
 	/**
 	 * 13. Scrabble Score
-	 * 
+	 * asjldfajksdfljk
 	 * Given a word, compute the scrabble score for that word.
 	 * 
 	 * --Letter Values-- Letter Value A, E, I, O, U, L, N, R, S, T = 1; D, G = 2; B,
